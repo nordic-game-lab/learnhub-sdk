a7ff04294d1a86d83961c9f8af6180347297b4df
Andrew Ball <andrew@nordicgamelab.org>
2024-01-16 15:56:42 +0000

    github actions

--- javascript-client-generated/src/index.ts
+++ javascript-client-generated/src/index.ts
@@ -1,29 +1,56 @@
 import axios from 'axios';
+import sentry from '@sentry/node';
+
+sentry.init({
+    dsn: "https://50d8e7d5c25b42a89803e62b07182065@o4505149596303360.ingest.sentry.io/4505150012391424",
+    tracesSampleRate: 1.0,
+    release: "ngl-api-sdk@" + process.env.npm_package_version
+  });
+  
+  
 
 async function getUser(apikey, email){
-    let response = await axios.get(`https://api.nordicgamelab.org/v2/user?email=${email}`, {
-        Headers: {
-            'Authorization': apikey
-        }
-    })
-    return response.data
+    try {
+        let response = await axios.get(`https://api.nordicgamelab.org/v2/user?email=${email}`, {
+            Headers: {
+                'Authorization': apikey
+            }
+        })
+        return response.data
+    } catch(e){
+        throw e;
+    }
 } 
 
 async function deleteUser(apikey, email){
-    let response = await axios.delete(`https://api.nordicgamelab.org/v2/user?email=${email}`, {
-        Headers: {
-            'Authorization': apikey
-        }
-    })
-    return response.data
+    try {
+        let response = await axios.delete(`https://api.nordicgamelab.org/v2/user?email=${email}`, {
+            Headers: {
+                'Authorization': apikey
+            }
+        })
+        return response.data
+    } catch(e){
+        throw e;
+    }
 } 
 
 async function createUser(apikey, body){
-    let response = await axios.post(`https://api.nordicgamelab.org/v2/user`, {
-        Headers: {
-            'Authorization': apikey
-        },
-        body: body
-    })
-    return response.data
+    try{
+        let response = await axios.post(`https://api.nordicgamelab.org/v2/user`, {
+            Headers: {
+                'Authorization': apikey
+            },
+            body: body
+        })
+        return response.data
+    } catch(e){
+        throw e;
+    }
 } 
+
+export {
+    getUser,
+    deleteUser,
+    createUser
+}
