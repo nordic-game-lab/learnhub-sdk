feafca5bb8c448bf8efda40cbafbf140a5370626
Andrew Ball <andrew@nordicgamelab.org>
2024-02-11 20:54:52 +0000

    add new endpoints

--- javascript-client-generated/src/index.ts
+++ javascript-client-generated/src/index.ts
@@ -30,6 +30,14 @@ export class Client {
         return this.createUser;
     }
 
+    get getendpoint(){
+        return this.getEndpoint;
+    }
+
+    get postendpoint(){
+        return this.postEndpoint;
+    }
+
     async getUser(email: string){
         try {
             let response = await axios.get(`${this.baseURL}/v2/user?email=${email}`, {
@@ -69,5 +77,32 @@ export class Client {
             throw e;
         }
     } 
+
+    async getEndpoint(endpoint: string){
+        try {
+            let response = await axios.get(`${this.baseURL}/${endpoint}`, {
+                headers: {
+                    'Authorization': this.token
+                }
+            })
+            return response.data
+        } catch(e){
+            throw e;
+        }
+    }
+
+    async postEndpoint(endpoint: string, body){
+        try {
+            let response = await axios.post(`${this.baseURL}/${endpoint}`, {
+                headers: {
+                    'Authorization': this.token
+                },
+                body: body
+            })
+            return response.data
+        } catch(e){
+            throw e;
+        }
+    }
 
 }
