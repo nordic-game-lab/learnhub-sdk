e0288340d66829124f2051afdf1bd3076236408d
Andrew <andrew@nordicgamelab.org>
2024-02-12 15:26:35 -0500

    Update index.ts

--- javascript-client-generated/src/index.ts
+++ javascript-client-generated/src/index.ts
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import sentry from '@sentry/node';
-import 'dotenv/config';
 
 sentry.init({
     dsn: "https://50d8e7d5c25b42a89803e62b07182065@o4505149596303360.ingest.sentry.io/4505150012391424",
