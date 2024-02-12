874206ef0263e107968a7eff81c5d4b9c6c0bb09
Andrew <andrew@nordicgamelab.org>
2024-02-12 15:35:23 -0500

    Update index.ts

--- javascript-client-generated/src/index.ts
+++ javascript-client-generated/src/index.ts
@@ -1,11 +1,4 @@
 import axios from 'axios';
-import sentry from '@sentry/node';
-
-sentry.init({
-    dsn: "https://50d8e7d5c25b42a89803e62b07182065@o4505149596303360.ingest.sentry.io/4505150012391424",
-    tracesSampleRate: 1.0,
-    release: "learnHub-Sdk@" + process.env.npm_package_version
-});
 
 export class Client {
     token: string;
